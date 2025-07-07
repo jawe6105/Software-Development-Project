df17b92dd5b97bc2fe1442a11a7d7a6c1a7fe6e0
jawe6105 <jawe6105@colorado.edu>
2025-07-06 19:55:53 -0600

    HBS Version of HTML

---
+++ HBS files/index.js
@@ -0,0 +1,94 @@
+// ----------------------------------   DEPENDENCIES  ----------------------------------------------
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars');
+const path = require('path');
+const pgp = require('pg-promise')();
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+// -------------------------------------  APP CONFIG   ----------------------------------------------
+
+// create `ExpressHandlebars` instance and configure the layouts and partials dir.
+const hbs = handlebars.create({
+    extname: 'hbs',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+});
+
+// Register `hbs` as our view engine using its bound `engine()` function.
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.json());
+// set Session
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        saveUninitialized: true,
+        resave: true,
+    })
+);
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+// -------------------------------------  DB CONFIG AND CONNECT   ---------------------------------------
+const dbConfig = {
+    host: 'db',
+    port: 5432,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+};
+const db = pgp(dbConfig);
+
+// db test
+db.connect()
+    .then(obj => {
+        // Can check the server version here (pg-promise v10.1.0+):
+        console.log('Database connection successful');
+        obj.done(); // success, release the connection;
+    })
+    .catch(error => {
+        console.log('ERROR', error.message || error);
+    });
+
+
+
+
+//FOR HOME
+app.get('/home', (req, res) => {
+  const claimed = req.query.claimed;
+  // Query to list all the jobs taken by a student
+
+  db.any(claimed ? job_id : all_jobs, [req.session.user.login_id])
+    .then(home => {
+      console.log(jobs)
+      res.render('pages/home', {
+        username: user.username,
+        jobs,
+        action: req.query.claimed ? 'delete' : 'add',
+      });
+    })
+    .catch(err => {
+      res.render('pages/home', {
+        jobs: [],
+        username: user.username,
+        error: true,
+        message: err.message,
+      });
+    });
+});
+
+
+
+app.post('/home/add', (req, res) => {
+    const claimed = parseInt(req.body.login_id);
+    db.tx(async t => {
+        // T
+        const {  } = await t.one(
+
+        );

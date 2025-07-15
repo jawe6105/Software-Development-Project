8fbe9e3c3749c750e74df5a173534e93c8e8c4a2
Mason Jones <majo1622@colorado.edu>
2025-07-15 15:11:03 -0600

    Updated create.sql

--- init_data/create.sql
+++ init_data/create.sql
@@ -1,22 +1,19 @@
-CREATE TABLE IF NOT EXISTS login_table (
-	login_id INT PRIMARY KEY NOT NULL,
-	username VARCHAR(100) PRIMARY KEY NOT NULL,
-	password VARCHAR(100) NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS user_data (
-	login_id INT NOT NULL,
-	username VARCHAR(100) NOT NULL,
+	login_id UNIQUE INT NOT NULL,
+	username UNIQUE VARCHAR(100) NOT NULL,
 	profile_picture VARCHAR(200),
-	FOREIGN KEY (login_id) REFERENCES login_table(login_id),
-	FOREIGN KEY (username) REFERENCES login_table(username),
+	password VARCHAR(100) NOT NULL,
 );
 
 CREATE TABLE IF NOT EXISTS jobs (
-	job_id INT NOT NULL,
+	job_id UNIQUE INT NOT NULL,
 	posted_by VARCHAR(100) NOT NULL,
-	job_description VARCHAR (1000) NOT NULL,
+	job_description VARCHAR(1000) NOT NULL,
+	job_image VARCHAR(100),
+	job_title VARCHAR(100) NOT NULL,
+	job_date VARCHAR(100) NOT NULL,
 	pay SMALLINT NOT NULL,
 	claimed BOOLEAN NOT NULL,
+	completed BOOLEAN NOT NULL,
 	claimed_by VARCHAR(100)
 );
